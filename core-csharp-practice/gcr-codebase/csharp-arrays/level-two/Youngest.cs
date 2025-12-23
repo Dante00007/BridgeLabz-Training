a66e9bdd5fc54dd4b2a3df2a865208d28fe266c6
Krishna Sharma <ks1658759@gmail.com>
2025-12-23 16:18:14 +0530

    Lab Assignment 23-12-2025

--- core-csharp-practice/gcr-codebase/csharp-arrays/level-two/Youngest.cs
+++ core-csharp-practice/gcr-codebase/csharp-arrays/level-two/Youngest.cs
@@ -1,10 +1,3 @@
-/*
-Create a program to find the youngest friends among 3 Amar, Akbar, and Anthony based on their ages and the tallest among the friends based on their heights
-Hint => 
-Take user input for age and height for the 3 friends and store it in two arrays each to store the values for age and height of the 3 friends
-Loop through the array and find the youngest of the 3 friends and the tallest of the 3 friends
-Finally display the youngest and tallest of the 3 friends
-*/
 
 class Youngest
 {
