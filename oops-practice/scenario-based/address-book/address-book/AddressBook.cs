466a7311dce999b87d5095deb8705a2d20dcc46a
Krishna Sharma <ks1658759@gmail.com>
2026-01-15 10:56:35 +0530

    Contact class made UC-1 Complete

---
+++ oops-practice/scenario-based/address-book/address-book/AddressBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace address_book {
+    internal class AddressBook {
+        static void Main() {
+            Contact contact = new Contact(
+                    "Bhanu",
+                    "Dagur",
+                    "JADANA",
+                    "Mathura",
+                    "U.P",
+                    "1234567890",
+                    "9t5Qm@example.com",
+                    "202148"
+                );
+            Console.WriteLine(contact);
+        }
+    }
+}
---
+++ oops-practice/scenario-based/address-book/address-book/Contact.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace address_book {
+    internal class Contact {
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string state;
+        private string phone;
+        private string email;
+        private string zip;
+
+        public Contact(string firstName, string lastName, string address, string city, string state, string phone, string email, string zip) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.phone = phone;
+            this.email = email;
+            this.zip = zip;
+        }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Address { get => address; set => address = value; }
+        public string City { get => city; set => city = value; }
+        public string State { get => state; set => state = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public string Email { get => email; set => email = value; }
+        public string Zip { get => zip; set => zip = value; }
+
+        public override string ToString() {
+            return $"Name : {FirstName} {LastName}\nAddress : {Address}\nCity : {City}\nState : {State}\nZip : {Zip}\nPhone : {Phone}\nEmail : {Email}";
+        }
+    }
+}
